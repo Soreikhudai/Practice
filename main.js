1245e43498356dd566d063ba36fc69c5f6a117be
Soreikhudai <khudaisoreipam@gmail.com>
2023-01-14 13:01:17 +0530

    created

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var item=document.getElementsByClassName('list-group-item')
+item[2].style.backgroundColor = 'green'
+for(let i=0; i<item.length; i++)
+{
+    item[i].style.fontWeight = 'bold';
+}
+
+
+
+
+
+
+
+
+
+
+
